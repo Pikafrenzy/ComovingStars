9faf072ddf30a4484d56e6202a3bcd25a8c1fd0b
Pikafrenzy <39431345+Pikafrenzy@users.noreply.github.com>
2025-01-01 20:33:29 -0500

    generate star pairs
    Added code to generate the initial conditions for each pair of stars

--- initConditionGenerator.py
+++ initConditionGenerator.py
@@ -94,3 +94,88 @@ if(saveGraphs):
 distPath = dirPath+"/Distribution_"+datetime.now().strftime("%Y%m%d_%H%M%S")+".png"
 if(saveGraphs): 
     plt.savefig(distPath)
+    
+    
+def pairGenerator(starCentre):
+    
+    xDiff = rng.normal(0,0.001)
+    x1 = starCentre.get_x().to_value() + xDiff
+    x2 = starCentre.get_x().to_value() - xDiff
+    
+    yDiff = rng.normal(0,0.001)
+    y1 = starCentre.get_y().to_value() + yDiff
+    y2 = starCentre.get_y().to_value() - yDiff
+    
+    zDiff = rng.normal(0,0.001)
+    z1 = starCentre.get_z().to_value() + zDiff
+    z2 = starCentre.get_z().to_value() - zDiff
+    
+    vxDiff = rng.normal(0,0.1)
+    vx1 = starCentre.get_Vx().to_value() + vxDiff
+    vx2 = starCentre.get_Vx().to_value() - vxDiff
+    
+    vyDiff = rng.normal(0,0.1)
+    vy1 = starCentre.get_Vy().to_value() + vyDiff
+    vy2 = starCentre.get_Vy().to_value() - vyDiff
+    
+    vzDiff = rng.normal(0,0.1)
+    vz1 = starCentre.get_Vz().to_value() + vzDiff
+    vz2 = starCentre.get_Vz().to_value() - vzDiff
+    
+    star1 = Star(x1,y1,z1,vx1,vy1,vz1)
+    star2 = Star(x2,y2,z2,vx2,vy2,vz2)
+    return [star1, star2]
+
+starPairs = []
+for star in stars:
+    starPair = pairGenerator(star)
+    starPairs.append(starPair)
+
+diff_X = []
+diff_Y = []
+diff_Z = []
+diff_Vx = []
+diff_Vy = []
+diff_Vz = []
+
+for starPair in starPairs:
+    diff_X.append((starPair[1].get_x()-starPair[0].get_x()).to_value())
+    diff_Y.append((starPair[1].get_y()-starPair[0].get_y()).to_value())
+    diff_Z.append((starPair[1].get_z()-starPair[0].get_z()).to_value())
+    diff_Vx.append((starPair[1].get_Vx()-starPair[0].get_Vx()).to_value())
+    diff_Vy.append((starPair[1].get_Vy()-starPair[0].get_Vy()).to_value())
+    diff_Vz.append((starPair[1].get_Vz()-starPair[0].get_Vz()).to_value())
+    
+fig2 = plt.figure(figsize = (40,40),layout = 'constrained')
+
+axPairXY = plt.subplot(221)
+axPairXY.scatter(diff_X,diff_Y)
+axPairXY.set_title("Difference in Y against X")
+axPairXY.tick_params('x',labelbottom = False)
+axPairXY.set_ylabel("y (kpc)")
+axPairXY.tick_params('both',length = 30)
+
+axPairXZ = plt.subplot(223, sharex = axPairXY)
+axPairXZ.scatter(diff_X,diff_Z)
+axPairXZ.set_title("Difference in Z against X")
+axPairXZ.set_ylabel("z (kpc)")
+axPairXZ.set_xlabel("x (kpc)")
+axPairXZ.tick_params('both',length = 30)
+
+axPairVelXY = plt.subplot(222)
+axPairVelXY.scatter(diff_Vx,diff_Vy)
+axPairVelXY.set_title("Difference in $v_y$ against $v_x$")
+axPairVelXY.tick_params('x',labelbottom = False)
+axPairVelXY.set_ylabel("y (km/s)")
+axPairVelXY.tick_params('both',length = 30)
+
+axPairVelXZ = plt.subplot(224)
+axPairVelXZ.scatter(diff_Vx,diff_Vz)
+axPairVelXZ.set_title("Difference in $v_z$ against $v_x$")
+axPairVelXZ.set_ylabel("z (km/s)")
+axPairVelXZ.set_xlabel("x (km/s)")
+axPairVelXZ.tick_params('both',length = 30)
+
+diffPath = dirPath+"/Difference_"+datetime.now().strftime("%Y%m%d_%H%M%S")+".png"
+if(saveGraphs): 
+    plt.savefig(diffPath)
